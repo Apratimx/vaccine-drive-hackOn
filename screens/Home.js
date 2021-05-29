a899fc56c7faabb71f6567312357684d43d0844b
Apratim <73159293+Apratimx@users.noreply.github.com>
2021-05-29 07:21:26 +0545

    initial commit

---
+++ screens/Home.js
@@ -0,0 +1,504 @@
+import React from "react";
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    FlatList
+} from "react-native";
+
+import { icons, images, SIZES, COLORS, FONTS } from '../constants'
+
+const Home = ({ navigation }) => {
+
+    // Dummy Datas
+
+    const initialCurrentLocation = {
+        streetName: "Bangalore",
+        gps: {
+            latitude: 1.5496614931250685,
+            longitude: 110.36381866919922
+        }
+    }
+
+    const categoryData = [
+        {
+            id: 1,
+            name: "Bangalore",
+            icon: icons.bangalore,
+        },
+        {
+            id: 2,
+            name: "Delhi",
+            icon: icons.delhi,
+        },
+        {
+            id: 3,
+            name: "Mumbai",
+            icon: icons.mumbai,
+        },
+        {
+            id: 4,
+            name: "Kolkata",
+            icon: icons.kolkata,
+        }
+
+    ]
+
+    // price rating
+    const affordable = 1
+    const fairPrice = 2
+    const expensive = 3
+
+    const restaurantData = [
+        {
+            id: 1,
+            name: "Bangalore Vaccine Heist",
+            rating: 4.8,
+            categories: [1],
+            priceRating: affordable,
+            photo: images.bangalore,
+            duration: "Sanjna",
+            location: {
+                latitude: 1.5347282806345879,
+                longitude: 110.35632207358996,
+            },
+            courier: {
+                avatar: images.avatar_1,
+                name: "Amy"
+            },
+            menu: [
+                {
+                    menuId: 1,
+                    name: "Crispy Chicken Burger",
+                    photo: images.crispy_chicken_burger,
+                    description: "Burger with crispy chicken, cheese and lettuce",
+                    calories: 200,
+                    price: 10
+                },
+                {
+                    menuId: 2,
+                    name: "Crispy Chicken Burger with Honey Mustard",
+                    photo: images.honey_mustard_chicken_burger,
+                    description: "Crispy Chicken Burger with Honey Mustard Coleslaw",
+                    calories: 250,
+                    price: 15
+                },
+                {
+                    menuId: 3,
+                    name: "Crispy Baked French Fries",
+                    photo: images.baked_fries,
+                    description: "Crispy Baked French Fries",
+                    calories: 194,
+                    price: 8
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: "Delhi Jab Machine",
+            categories: [2],
+            photo: images.DelhiH,
+            duration: "Apratim",
+            location: {
+                latitude: 1.556306570595712,
+                longitude: 110.35504616746915,
+            },
+            courier: {
+                avatar: images.avatar_2,
+                name: "Jackson"
+            },
+            menu: [
+                {
+                    menuId: 4,
+                    name: "Hawaiian Pizza",
+                    photo: images.hawaiian_pizza,
+                    description: "Canadian bacon, homemade pizza crust, pizza sauce",
+                    calories: 250,
+                    price: 15
+                },
+                {
+                    menuId: 5,
+                    name: "Tomato & Basil Pizza",
+                    photo: images.pizza,
+                    description: "Fresh tomatoes, aromatic basil pesto and melted bocconcini",
+                    calories: 250,
+                    price: 20
+                },
+                {
+                    menuId: 6,
+                    name: "Tomato Pasta",
+                    photo: images.tomato_pasta,
+                    description: "Pasta with fresh tomatoes",
+                    calories: 100,
+                    price: 10
+                },
+                {
+                    menuId: 7,
+                    name: "Mediterranean Chopped Salad ",
+                    photo: images.salad,
+                    description: "Finely chopped lettuce, tomatoes, cucumbers",
+                    calories: 100,
+                    price: 10
+                }
+            ]
+        },
+        {
+            id: 3,
+            name: "Mumbai pokes itself",
+            rating: 4.8,
+            categories: [3],
+            priceRating: expensive,
+            photo: images.mumbai,
+            duration: "20 - 25 min",
+            location: {
+                latitude: 1.5238753474714375,
+                longitude: 110.34261833833622,
+            },
+            courier: {
+                avatar: images.avatar_3,
+                name: "James"
+            },
+            menu: [
+                {
+                    menuId: 8,
+                    name: "Chicago Style Hot Dog",
+                    photo: images.chicago_hot_dog,
+                    description: "Fresh tomatoes, all beef hot dogs",
+                    calories: 100,
+                }
+            ]
+        },
+        
+        {
+            id: 5,
+            name: "Delhi jab jab",
+            rating: 4.8,
+            categories: [2],
+            priceRating: affordable,
+            photo: images.DelhiM,
+            duration: "Karthik",
+            location: {
+                latitude: 1.558050496260768,
+                longitude: 110.34743759630511,
+            },
+            courier: {
+                avatar: images.avatar_4,
+                name: "Muthu"
+            },
+            menu: [
+                {
+                    menuId: 10,
+                    name: "Kolo Mee",
+                    photo: images.kolo_mee,
+                    description: "Noodles with char siu",
+                    calories: 200,
+                    price: 5
+                },
+                {
+                    menuId: 11,
+                    name: "Sarawak Laksa",
+                    photo: images.sarawak_laksa,
+                    description: "Vermicelli noodles, cooked prawns",
+                    calories: 300,
+                    price: 8
+                },
+                {
+                    menuId: 12,
+                    name: "Nasi Lemak",
+                    photo: images.nasi_lemak,
+                    description: "A traditional Malay rice dish",
+                    calories: 300,
+                    price: 8
+                },
+                {
+                    menuId: 13,
+                    name: "Nasi Briyani with Mutton",
+                    photo: images.nasi_briyani_mutton,
+                    description: "A traditional Indian rice dish with mutton",
+                    calories: 300,
+                    price: 8
+                },
+
+            ]
+        },
+    
+
+
+    ]
+
+    const [categories, setCategories] = React.useState(categoryData)
+    const [selectedCategory, setSelectedCategory] = React.useState(null)
+    const [restaurants, setRestaurants] = React.useState(restaurantData)
+    const [currentLocation, setCurrentLocation] = React.useState(initialCurrentLocation)
+
+
+    function onSelectCategory(category) {
+        //filter restaurant
+        let restaurantList = restaurantData.filter(a => a.categories.includes(category.id))
+
+        setRestaurants(restaurantList)
+
+        setSelectedCategory(category)
+    }
+
+    function getCategoryNameById(id) {
+        let category = categories.filter(a => a.id == id)
+
+        if (category.length > 0)
+            return category[0].name
+
+        return ""
+    }
+
+    function renderHeader() {
+        return (
+            <View style={{ flexDirection: 'row', height: 50 }}>
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingLeft: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.nearby}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <View
+                        style={{
+                            width: '70%',
+                            height: "100%",
+                            backgroundColor: COLORS.lightGray3,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            borderRadius: SIZES.radius
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h3 }}>{currentLocation.streetName}</Text>
+                    </View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        width: 50,
+                        paddingRight: SIZES.padding * 2,
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={icons.basket}
+                        resizeMode="contain"
+                        style={{
+                            width: 30,
+                            height: 30
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    function renderMainCategories() {
+        const renderItem = ({ item }) => {
+            return (
+                <TouchableOpacity
+                    style={{
+                        padding: SIZES.padding,
+                        paddingBottom: SIZES.padding * 2,
+                        backgroundColor: (selectedCategory?.id == item.id) ? COLORS.primary : COLORS.white,
+                        borderRadius: SIZES.radius,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginRight: SIZES.padding,
+                        ...styles.shadow
+                    }}
+                    onPress={() => onSelectCategory(item)}
+                >
+                    <View
+                        style={{
+                            width: 50,
+                            height: 50,
+                            borderRadius: 25,
+                            alignItems: "center",
+                            justifyContent: "center",
+                            backgroundColor: (selectedCategory?.id == item.id) ? COLORS.white : COLORS.lightGray
+                        }}
+                    >
+                        <Image
+                            source={item.icon}
+                            resizeMode="contain"
+                            style={{
+                                width: 30,
+                                height: 30
+                            }}
+                        />
+                    </View>
+
+                    <Text
+                        style={{
+                            marginTop: SIZES.padding,
+                            color: (selectedCategory?.id == item.id) ? COLORS.white : COLORS.black,
+                            ...FONTS.body5
+                        }}
+                    >
+                        {item.name}
+                    </Text>
+                </TouchableOpacity>
+            )
+        }
+
+        return (
+            <View style={{ padding: SIZES.padding * 2 }}>
+                <Text style={{ ...FONTS.h1 }}>Main</Text>
+                <Text style={{ ...FONTS.h1 }}>Categories</Text>
+
+                <FlatList
+                    data={categories}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={renderItem}
+                    contentContainerStyle={{ paddingVertical: SIZES.padding * 2 }}
+                />
+            </View>
+        )
+    }
+
+    function renderRestaurantList() {
+        const renderItem = ({ item }) => (
+            <TouchableOpacity
+                style={{ marginBottom: SIZES.padding * 2 }}
+                onPress={() => navigation.navigate("Restaurant", {
+                    item,
+                    currentLocation
+                })}
+            >
+                {/* Image */}
+                <View
+                    style={{
+                        marginBottom: SIZES.padding
+                    }}
+                >
+                    <Image
+                        source={item.photo}
+                        resizeMode="cover"
+                        style={{
+                            width: "100%",
+                            height: 200,
+                            borderRadius: SIZES.radius
+                        }}
+                    />
+
+                    <View
+                        style={{
+                            position: 'absolute',
+                            bottom: 0,
+                            height: 50,
+                            width: SIZES.width * 0.3,
+                            backgroundColor: COLORS.white,
+                            borderTopRightRadius: SIZES.radius,
+                            borderBottomLeftRadius: SIZES.radius,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            ...styles.shadow
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h4 }}>{item.duration}</Text>
+                    </View>
+                </View>
+
+                {/* Restaurant Info */}
+                <Text style={{ ...FONTS.body2 }}>{item.name}</Text>
+
+                <View
+                    style={{
+                        marginTop: SIZES.padding,
+                        flexDirection: 'row'
+                    }}
+                >
+                    {/* Rating */}
+                    <Image
+                        source={icons.pin}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            tintColor: COLORS.primary,
+                            marginRight: 10
+                        }}
+                    />
+
+                    {/* Categories */}
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            marginLeft: 10
+                        }}
+                    >
+                        {
+                            item.categories.map((categoryId) => {
+                                return (
+                                    <View
+                                        style={{ flexDirection: 'row' }}
+                                        key={categoryId}
+                                    >
+                                        <Text style={{ ...FONTS.body3 }}>{getCategoryNameById(categoryId)}</Text>
+                                        <Text style={{ ...FONTS.h3, color: COLORS.darkgray }}> . </Text>
+                                    </View>
+                                )
+                            })
+                        }
+                    </View>
+                </View>
+            </TouchableOpacity>
+        )
+
+        return (
+            <FlatList
+                data={restaurants}
+                keyExtractor={item => `${item.id}`}
+                renderItem={renderItem}
+                contentContainerStyle={{
+                    paddingHorizontal: SIZES.padding * 2,
+                    paddingBottom: 30
+                }}
+            />
+        )
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            {renderHeader()}
+            {renderMainCategories()}
+            {renderRestaurantList()}
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.lightGray4
+    },
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 1,
+    }
+})
+
+export default Home
